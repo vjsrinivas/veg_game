a4b6ab8b282fe65117ec5c47e1cfbcdda0d61e2e
Vijaysrinivas Rajagopal <vijaythinks@gmail.com>
2017-04-05 08:34:41 -0500

    Started with HUD of game; importing moves.json

--- client.js
+++ client.js
@@ -1,17 +1,20 @@
-function init() {
-    var stage = new createjs.Stage("demoCanvas");
-    var circle = new createjs.Shape();
-    circle.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, 50);
-    circle.x = 100;
-    circle.y = 100;
-    stage.addChild(circle);
-    createjs.Tween.get(circle, { loop: true })
-  .to({ x: 400 }, 1000, createjs.Ease.getPowInOut(4))
-  .to({ alpha: 0, y: 175 }, 500, createjs.Ease.getPowInOut(2))
-  .to({ alpha: 0, y: 225 }, 100)
-  .to({ alpha: 1, y: 200 }, 500, createjs.Ease.getPowInOut(2))
-  .to({ x: 100 }, 800, createjs.Ease.getPowInOut(2));
+var stage;
+var stats_raw = `{"player":{"attack":{"carrot_launch":{"max_dmg":50,"min_dmg":0,"chance":0.6,"canDouble":true},"carrot_beam":{"max_dmg":90,"min_dmg":30,"chance":0.5,"canDouble":false},"carrot_smash":{"max_dmg":75,"min_dmg":[15,30,45],"chance":0.4,"canDouble":false},"celery_crush":{"max_dmg":100,"min_dmg":70,"chance":0.6,"canDouble":false},"egg_pan":{"max_dmg":90,"min_dmg":60,"chance":0.3,"canDouble":true}},"defense":{"carrot_vitc":{"max_heal":20,"min_heal":10,"deflection":0.5,"reduction":0.5,"chance":1,"canMultiple":true},"egg_hard":{"max_heal":0,"min_heal":0,"deflection":1,"reduction":1,"chance":0.9,"canMultiple":true},"egg_selfharden":{"max_heal":0,"min_heal":0,"deflection":1,"reduction":1,"chance":0.4,"canMultiple":true},"egg_angel":{"max_heal":100,"min_heal":99,"deflection":0,"reduction":0,"chance":0.2,"canMultiple":false,"spreadAll":true},"celery_fortify":{"max_heal":0,"min_heal":0,"deflection":1,"reduction":0,"chance":0,"canMultiple":true}}},"enemy":{"attack":{}}}`;
+var stats = JSON.parse(stats_raw);
 
-  createjs.Ticker.setFPS(60);
-createjs.Ticker.addEventListener("tick", stage);
-  }
+function init() {
+  stage = new createjs.Stage("demoCanvas");
+  init_action_stage(stage);
+  stage.update();
+}
+
+function action_stage_visibility(event){
+  alert("hi!");
+}
+
+function init_action_stage(stage){
+  var action_stage = new createjs.Shape();
+  action_stage.graphics.beginFill("red").drawRect(20,20,300,75);
+  stage.addChild(action_stage);
+  stage.addEventListener("click", action_stage_visibility)
+}
