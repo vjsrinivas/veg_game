0098c3ccf6a26a94deb697ced5cbcf65e2f44fde
Vijaysrinivas Rajagopal <vijaythinks@gmail.com>
2017-04-12 08:25:37 -0500

    changed stuff
    changed game icons, added character data, reorganized json

--- client.js
+++ client.js
@@ -1,8 +1,8 @@
 var stage;
-var stats_raw = `{"player":{"attack":{"carrot_launch":{"max_dmg":50,"min_dmg":0,"chance":0.6,"canDouble":true},"carrot_beam":{"max_dmg":90,"min_dmg":30,"chance":0.5,"canDouble":false},"carrot_smash":{"max_dmg":75,"min_dmg":[15,30,45],"chance":0.4,"canDouble":false},"celery_crush":{"max_dmg":100,"min_dmg":70,"chance":0.6,"canDouble":false},"egg_pan":{"max_dmg":90,"min_dmg":60,"chance":0.3,"canDouble":true}},"defense":{"carrot_vitc":{"max_heal":20,"min_heal":10,"deflection":0.5,"reduction":0.5,"chance":1,"canMultiple":true},"egg_hard":{"max_heal":0,"min_heal":0,"deflection":1,"reduction":1,"chance":0.9,"canMultiple":true},"egg_selfharden":{"max_heal":0,"min_heal":0,"deflection":1,"reduction":1,"chance":0.4,"canMultiple":true},"egg_angel":{"max_heal":100,"min_heal":99,"deflection":0,"reduction":0,"chance":0.2,"canMultiple":false,"spreadAll":true},"celery_fortify":{"max_heal":0,"min_heal":0,"deflection":1,"reduction":0,"chance":0,"canMultiple":true}}},"enemy":{"attack":{}}}`;
+var stats_raw = `{"player":{"attack":{"carrot":{"carrot_launch":{"max_dmg":50,"min_dmg":0,"chance":0.6,"canDouble":true},"carrot_beam":{"max_dmg":90,"min_dmg":30,"chance":0.5,"canDouble":false},"carrot_smash":{"max_dmg":75,"min_dmg":[15,30,45],"chance":0.4,"canDouble":false}},"celery":{"celery_crush":{"max_dmg":100,"min_dmg":70,"chance":0.6,"canDouble":false}},"egg":{"egg_pan":{"max_dmg":90,"min_dmg":60,"chance":0.3,"canDouble":true}}},"defense":{"carrot":{"carrot_vitc":{"max_heal":20,"min_heal":10,"deflection":0.5,"reduction":0.5,"chance":1,"canMultiple":true}},"egg":{"egg_hard":{"max_heal":0,"min_heal":0,"deflection":1,"reduction":1,"chance":0.9,"canMultiple":true},"egg_selfharden":{"max_heal":0,"min_heal":0,"deflection":1,"reduction":1,"chance":0.4,"canMultiple":true},"egg_angel":{"max_heal":100,"min_heal":99,"deflection":0,"reduction":0,"chance":0.2,"canMultiple":false,"spreadAll":true}},"celery":{"celery_fortify":{"max_heal":0,"min_heal":0,"deflection":1,"reduction":0,"chance":0,"canMultiple":true}}}},"enemy":{"attack":{}}}`
 var stats = JSON.parse(stats_raw);
 var current_moves = []
-var carrot;
+var carrot = init_characters(100, stats["player"]["attack"]["carrot"]);
 
 function init() {
   stage = new createjs.Stage("demoCanvas");
@@ -97,11 +97,20 @@ function init_action_move(stage, move, x, y){
     evt.currentTarget.y = evt.stageY;
     stage.update();
   })
+}
 
   /////
 
-function init_characters(){
-
+function init_characters(para_hp, moves){
+  var character = {hp: para_hp, moves: moves};
+  return character;
 }
 
-}
+
+/*function init_playbutton(){
+  var helper = new createjs.ButtonHelper(myInstance, "out", "over", "down", false, myInstance, "hit");
+  myInstance.addEventListener("click", handleClick);
+  function handleClick(event) {
+     alert("afaf");
+ }
+}*/
